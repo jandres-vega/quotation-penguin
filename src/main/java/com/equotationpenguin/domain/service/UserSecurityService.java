f00117198f8b4ba162f0eb3429dd719379bdb4e5
jandres-vega <jandresvega@itsa.edu.co>
2024-04-18 08:36:40 -0500

    Add service UserSecurity of domain

---
+++ src/main/java/com/equotationpenguin/domain/service/UserSecurityService.java
@@ -0,0 +1,28 @@
+package com.equotationpenguin.domain.service;
+
+import com.pinguinera.domain.dto.models.CustomerDTO;
+import com.pinguinera.persistence.repository.CustomerRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    public UserSecurityService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        CustomerDTO userEntity = customerRepository.findCustomerByEmail(email);
+        return User.builder()
+                .username(userEntity.getEmail())
+                .password(userEntity.getPassword())
+                .roles("CUSTOMER")
+                .build();
+    }
+}
