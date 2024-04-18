7dd222b598ba08b7225acd1c2e0781399a07a1ef
jandres-vega <jandresvega@itsa.edu.co>
2024-04-18 08:40:07 -0500

    Add service filter service of domain

---
+++ src/main/java/com/equotationpenguin/domain/calculate/filterBooks/FilterBooks.java
@@ -0,0 +1,33 @@
+package com.equotationpenguin.domain.calculate.filterBooks;
+
+import com.pinguinera.domain.dto.response.BookResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class FilterBooks {
+
+    public BookResponse findCheapestNovel(List<BookResponse> copies) {
+        return copies.stream()
+                .filter(copy -> copy.getCategory().getIdCategory() == 2)
+                .min(Comparator.comparingDouble(BookResponse::getPrice))
+                .orElse(null);
+    }
+
+    public BookResponse findCheapestBook(List<BookResponse> copies) {
+        return copies.stream()
+                .filter(copy -> copy.getCategory().getIdCategory() == 1)
+                .min(Comparator.comparingDouble(BookResponse::getPrice))
+                .orElse(null);
+    }
+
+    private BookResponse  getCheapestCopy(BookResponse copy1, BookResponse  copy2) {
+        return copy1.getPrice() < copy2.getPrice() ? copy1 : copy2;
+    }
+
+    public BookResponse findCheapestCopy(BookResponse  cheapestNovel, BookResponse  cheapestBook) {
+        return getCheapestCopy(cheapestNovel, cheapestBook);
+    }
+}
