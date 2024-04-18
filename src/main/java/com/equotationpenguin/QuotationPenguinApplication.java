b50b81d7d408ed6698728abb311ec57021bd7836
jandres-vega <jandresvega@itsa.edu.co>
2024-04-17 22:48:54 -0500

    first commit

---
+++ src/main/java/com/equotationpenguin/QuotationPenguinApplication.java
@@ -0,0 +1,13 @@
+package com.equotationpenguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationPenguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationPenguinApplication.class, args);
+	}
+
+}
